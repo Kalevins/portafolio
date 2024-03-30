e57a7cc004483365e14f0b66fccf31876bbc10f8
Kevin Muñoz Rengifo <kevinmunoz01@hotmail.com>
2024-03-30 15:52:28 -0500

    [Fix] base url vite config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 /* eslint-disable no-undef */
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd())
   const config = {
     resolve:{
       alias: {
@@ -34,7 +35,7 @@ export default defineConfig(({ command }) => {
   }
 
   if (command !== 'serve') {
-    config.base = import.meta.env.VITE_BASE_URL
+    config.base = env.VITE_BASE_URL
   }
 
   return config
