3d500bd44177c17e1436726d05f3461d902e0664
Kevin Muñoz Rengifo <kevinmunoz01@hotmail.com>
2024-03-30 15:40:10 -0500

    [Modify] base url

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
@@ -5,17 +6,35 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
   const config = {
-    plugins: [react()],
-    base: '/',
-    resolve: {
+    resolve:{
       alias: {
         '@': path.resolve(__dirname, './src/'),
-      },
+        assets: path.resolve(__dirname, './src/assets/'),
+        components: path.resolve(__dirname, './src/components/'),
+        containers: path.resolve(__dirname, './src/containers/'),
+        context: path.resolve(__dirname, './src/context/'),
+        hooks: path.resolve(__dirname, './src/hooks/'),
+        pages: path.resolve(__dirname, './src/pages/'),
+        routes: path.resolve(__dirname, './src/routes/'),
+        services: path.resolve(__dirname, './src/services/'),
+        styles: path.resolve(__dirname, './src/styles/'),
+        utils: path.resolve(__dirname, './src/utils/')
+      }
     },
+    plugins: [react()],
+    test: {
+      environment: 'jsdom',
+      setupFiles: './setupTest.js'
+    },
+    server: {
+      host: true,
+      port: 3000
+    },
+    base: '/',
   }
 
   if (command !== 'serve') {
-    config.base = '/Portafolio/'
+    config.base = import.meta.env.VITE_BASE_URL
   }
 
   return config
