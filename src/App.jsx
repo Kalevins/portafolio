2644f5de93b8bd23f54f180800d8dfcb017e0766
Kevin Muñoz Rengifo <kevinmunoz01@hotmail.com>
2024-04-18 12:04:07 -0500

    [Delete] project

--- src/App.jsx
+++
@@ -1,6 +0,0 @@
-export function App() {
-
-  return (
-    <h1>Hello World</h1>
-  )
-}
--- src/components/LoadingScreen/index.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './styles.module.scss';
-
-export const LoadingScreen = () => {
-  return (
-    <main className={styles.main} data-testid={'loadingScreen'}>
-      <div className={styles.spinner} />
-    </main>
-  )
-}
