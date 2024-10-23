e59d31a5d5330dac97e8c79d47c3874a66cb544c
Kevin Muñoz Rengifo <kevinmunoz01@hotmail.com>
2024-10-23 13:27:50 -0500

    [Update] Reorder technologies

--- src/utils/technologies.ts
+++ src/utils/technologies.ts
@@ -1,213 +1,182 @@
 import {
-  // Languages
-  LogosJavascript,
-  LogosTypescriptIcon,
-  LogosPython,
-  LogosCPlusplus,
-  // Frameworks
-  LogosReact,
   LogosAngularIcon,
-  LogosNextjsIcon,
-  LogosNodejsIconAlt,
-  // Frontend
-  LogosHtml5,
-  LogosCss3,
-  LogosSass,
-  VscodeIconsFileTypeStyled,
-  LogosTailwindcssIcon,
-  LogosMaterialUi,
+  LogosApollostack,
+  LogosAuth0Icon,
+  LogosAws,
+  LogosBitbucket,
   LogosBootstrap,
-  SimpleIconsShadcnui,
-  // Libraries
-  LogosVitejs,
-  LogosRedux,
-  LogosZustand,
   LogosChartjs,
+  LogosCPlusplus,
+  LogosCss3,
+  LogosDockerIcon,
+  LogosExpress,
+  LogosFigma,
+  LogosFirebase,
   LogosFramer,
-  SimpleIconsI18next,
-  SimpleIconsReacthookform,
-  LogosReactRouter,
-  LogosOpencv,
-  VscodeIconsFileTypeDoxygen,
-  // Testing
-  LogosVitest,
+  LogosGitIcon,
+  LogosGithubCopilot,
+  LogosGithubIcon,
+  LogosGithubOctocat,
+  LogosGraphql,
+  LogosHtml5,
+  LogosJavascript,
   LogosJest,
   LogosKarma,
-  LogosMswIcon,
-  LogosPostmanIcon,
-  // Backend
-  LogosExpress,
-  LogosNestjs,
-  LogosFirebase,
-  LogosSupabaseIcon,
-  LogosAuth0Icon,
-  LogosPrisma,
-  LogosGraphql,
-  LogosApollostack,
-  // QA
-  SimpleIconsZap,
-  LogosSonarqube,
-  // Version Control
-  LogosGitIcon,
-  LogosGithubIcon,
-  LogosBitbucket,
-  LogosGithubCopilot,
-  // Cloud
-  LogosAws,
-  LogosMicrosoftAzure,
-  LogosVercelIcon,
-  LogosGithubOctocat,
-  LogosDockerIcon,
   LogosKubernetes,
-  LogosNginx,
-  // Operating Systems
-  LogosMicrosoftWindowsIcon,
   LogosLinuxTux,
-  // Design
-  LogosFigma,
+  LogosMaterialUi,
+  LogosMicrosoftAzure,
+  LogosMicrosoftWindowsIcon,
+  LogosMswIcon,
+  LogosNestjs,
+  LogosNextjsIcon,
+  LogosNginx,
+  LogosNodejsIconAlt,
+  LogosOpencv,
+  LogosPostmanIcon,
+  LogosPrisma,
+  LogosPython,
+  LogosReact,
+  LogosReactRouter,
+  LogosRedux,
+  LogosSass,
+  LogosSonarqube,
+  LogosSupabaseIcon,
+  LogosTailwindcssIcon,
+  LogosTypescriptIcon,
+  LogosVercelIcon,
+  LogosVitest,
+  LogosVitejs,
+  LogosZustand,
+  SimpleIconsI18next,
+  SimpleIconsReacthookform,
+  SimpleIconsShadcnui,
+  SimpleIconsZap,
+  VscodeIconsFileTypeDoxygen,
+  VscodeIconsFileTypeStyled,
 } from "@/assets/icons"
 import { Technologies } from "@/types"
 
 export const technologies: Technologies = {
-  // Languages
-  JavaScript: {
-    name: "Java Script",
-    url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
-    icon: LogosJavascript
-  },
-  TypeScript: {
-    name: "Type Script",
-    url: "https://www.typescriptlang.org/",
-    icon: LogosTypescriptIcon
-  },
-  Python: {
-    name: "Python",
-    url: "https://www.python.org/",
-    icon: LogosPython
-  },
-  Cplusplus: {
-    name: "C++",
-    url: "https://isocpp.org/",
-    icon: LogosCPlusplus
-  },
-  // Frameworks
-  React: {
-    name: "React",
-    url: "https://reactjs.org/",
-    icon: LogosReact
-  },
   Angular: {
     name: "Angular",
     url: "https://angular.io/",
     icon: LogosAngularIcon
   },
-  Nextjs: {
-    name: "Next.js",
-    url: "https://nextjs.org/",
-    icon: LogosNextjsIcon
+  Apollo: {
+    name: "Apollo",
+    url: "https://www.apollographql.com/",
+    icon: LogosApollostack
   },
-  Nodejs: {
-    name: "Node.js",
-    url: "https://nodejs.org/",
-    icon: LogosNodejsIconAlt
+  Auth0: {
+    name: "Auth0",
+    url: "https://auth0.com/",
+    icon: LogosAuth0Icon
   },
-  // Frontend
-  HTML: {
-    name: "HTML",
-    url: "https://developer.mozilla.org/en-US/docs/Web/HTML",
-    icon: LogosHtml5
+  AWS: {
+    name: "AWS",
+    url: "https://aws.amazon.com/",
+    icon: LogosAws
   },
-  CSS: {
-    name: "CSS",
-    url: "https://developer.mozilla.org/en-US/docs/Web/CSS",
-    icon: LogosCss3
+  Azure: {
+    name: "Microsoft Azure",
+    url: "https://azure.microsoft.com/",
+    icon: LogosMicrosoftAzure
   },
-  SASS: {
-    name: "SASS",
-    url: "https://sass-lang.com/",
-    icon: LogosSass
-  },
-  StyledComponents: {
-    name: "Styled Components",
-    url: "https://styled-components.com/",
-    icon: VscodeIconsFileTypeStyled
-  },
-  TailwindCSS: {
-    name: "Tailwind CSS",
-    url: "https://tailwindcss.com/",
-    icon: LogosTailwindcssIcon
-  },
-  MaterialUi: {
-    name: "Material UI",
-    url: "https://material-ui.com/",
-    icon: LogosMaterialUi
+  Bitbucket: {
+    name: "Bitbucket",
+    url: "https://bitbucket.org/",
+    icon: LogosBitbucket
   },
   Bootstrap: {
     name: "Bootstrap",
     url: "https://getbootstrap.com/",
     icon: LogosBootstrap
   },
-  ShadcnUi: {
-    name: "Shadcn UI",
-    url: "https://ui.shadcn.com/",
-    icon: SimpleIconsShadcnui
-  },
-  // Libraries
-  Vite: {
-    name: "Vite",
-    url: "https://vitejs.dev/",
-    icon: LogosVitejs
-  },
-  Redux: {
-    name: "Redux",
-    url: "https://redux.js.org/",
-    icon: LogosRedux
-  },
-  Zustand: {
-    name: "Zustand",
-    url: "https://zustand.surge.sh/",
-    icon: LogosZustand
-  },
   ChartJS: {
     name: "Chart.js",
     url: "https://www.chartjs.org/",
     icon: LogosChartjs
   },
-  Framer: {
-    name: "Framer Motion",
-    url: "https://www.framer.com/",
-    icon: LogosFramer
+  Cplusplus: {
+    name: "C++",
+    url: "https://isocpp.org/",
+    icon: LogosCPlusplus
   },
-  I18next: {
-    name: "i18next",
-    url: "https://www.i18next.com/",
-    icon: SimpleIconsI18next
+  CSS: {
+    name: "CSS",
+    url: "https://developer.mozilla.org/en-US/docs/Web/CSS",
+    icon: LogosCss3
   },
-  ReactHookForm: {
-    name: "React Hook Form",
-    url: "https://react-hook-form.com/",
-    icon: SimpleIconsReacthookform
-  },
-  ReactRouter: {
-    name: "React Router",
-    url: "https://reactrouter.com/",
-    icon: LogosReactRouter
-  },
-  OpenCV: {
-    name: "OpenCV",
-    url: "https://opencv.org/",
-    icon: LogosOpencv
+  Docker: {
+    name: "Docker",
+    url: "https://www.docker.com/",
+    icon: LogosDockerIcon
   },
   Doxygen: {
     name: "Doxygen",
     url: "https://www.doxygen.nl/index.html",
     icon: VscodeIconsFileTypeDoxygen
   },
-  // Testing
-  Vitest: {
-    name: "Vitest",
-    url: "https://vitest.dev/",
-    icon: LogosVitest
+  Express: {
+    name: "Express",
+    url: "https://expressjs.com/",
+    icon: LogosExpress
+  },
+  Figma: {
+    name: "Figma",
+    url: "https://www.figma.com/",
+    icon: LogosFigma
+  },
+  Firebase: {
+    name: "Firebase",
+    url: "https://firebase.google.com/",
+    icon: LogosFirebase
+  },
+  Framer: {
+    name: "Framer Motion",
+    url: "https://www.framer.com/",
+    icon: LogosFramer
+  },
+  Git: {
+    name: "Git",
+    url: "https://git-scm.com/",
+    icon: LogosGitIcon
+  },
+  GitHub: {
+    name: "GitHub",
+    url: "https://github.com/",
+    icon: LogosGithubIcon
+  },
+  GitHubCopilot: {
+    name: "GitHub Copilot",
+    url: "https://copilot.github.com/",
+    icon: LogosGithubCopilot
+  },
+  GitHubPages: {
+    name: "GitHub Pages",
+    url: "https://pages.github.com/",
+    icon: LogosGithubOctocat
+  },
+  GraphQL: {
+    name: "GraphQL",
+    url: "https://graphql.org/",
+    icon: LogosGraphql
+  },
+  HTML: {
+    name: "HTML",
+    url: "https://developer.mozilla.org/en-US/docs/Web/HTML",
+    icon: LogosHtml5
+  },
+  I18next: {
+    name: "i18next",
+    url: "https://www.i18next.com/",
+    icon: SimpleIconsI18next
+  },
+  JavaScript: {
+    name: "Java Script",
+    url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+    icon: LogosJavascript
   },
   Jest: {
     name: "Jest",
@@ -219,141 +188,150 @@ export const technologies: Technologies = {
     url: "https://karma-runner.github.io/",
     icon: LogosKarma
   },
-  MSW: {
-    name: "MSW",
-    url: "https://mswjs.io/",
-    icon: LogosMswIcon
-  },
-  Postman: {
-    name: "Postman",
-    url: "https://www.postman.com/",
-    icon: LogosPostmanIcon
-  },
-  // Backend
-  Express: {
-    name: "Express",
-    url: "https://expressjs.com/",
-    icon: LogosExpress
-  },
-  Nestjs: {
-    name: "Nest.js",
-    url: "https://nestjs.com/",
-    icon: LogosNestjs
-  },
-  Firebase: {
-    name: "Firebase",
-    url: "https://firebase.google.com/",
-    icon: LogosFirebase
-  },
-  Supabase: {
-    name: "Supabase",
-    url: "https://supabase.io/",
-    icon: LogosSupabaseIcon
-  },
-  Auth0: {
-    name: "Auth0",
-    url: "https://auth0.com/",
-    icon: LogosAuth0Icon
-  },
-  Prisma: {
-    name: "Prisma",
-    url: "https://www.prisma.io/",
-    icon: LogosPrisma
-  },
-  GraphQL: {
-    name: "GraphQL",
-    url: "https://graphql.org/",
-    icon: LogosGraphql
-  },
-  Apollo: {
-    name: "Apollo",
-    url: "https://www.apollographql.com/",
-    icon: LogosApollostack
-  },
-  // QA
-  OWASPZAP: {
-    name: "OWASP ZAP",
-    url: "https://www.zaproxy.org/",
-    icon: SimpleIconsZap
-  },
-  SonarQube: {
-    name: "Sonar Qube",
-    url: "https://www.sonarqube.org/",
-    icon: LogosSonarqube
-  },
-  // Version Control
-  Git: {
-    name: "Git",
-    url: "https://git-scm.com/",
-    icon: LogosGitIcon
-  },
-  GitHub: {
-    name: "GitHub",
-    url: "https://github.com/",
-    icon: LogosGithubIcon
-  },
-  Bitbucket: {
-    name: "Bitbucket",
-    url: "https://bitbucket.org/",
-    icon: LogosBitbucket
-  },
-  GitHubCopilot: {
-    name: "GitHub Copilot",
-    url: "https://copilot.github.com/",
-    icon: LogosGithubCopilot
-  },
-  // Cloud
-  AWS: {
-    name: "AWS",
-    url: "https://aws.amazon.com/",
-    icon: LogosAws
-  },
-  Azure: {
-    name: "Microsoft Azure",
-    url: "https://azure.microsoft.com/",
-    icon: LogosMicrosoftAzure
-  },
-  Vercel: {
-    name: "Vercel",
-    url: "https://vercel.com/",
-    icon: LogosVercelIcon
-  },
-  GitHubPages: {
-    name: "GitHub Pages",
-    url: "https://pages.github.com/",
-    icon: LogosGithubOctocat
-  },
-  Docker: {
-    name: "Docker",
-    url: "https://www.docker.com/",
-    icon: LogosDockerIcon
-  },
   Kubernetes: {
     name: "Kubernetes",
     url: "https://kubernetes.io/",
     icon: LogosKubernetes
-  },
-  Nginx: {
-    name: "Nginx",
-    url: "https://www.nginx.com/",
-    icon: LogosNginx
-  },
-  // Operating Systems
-  Windows: {
-    name: "Windows",
-    url: "https://www.microsoft.com/en-us/windows",
-    icon: LogosMicrosoftWindowsIcon
   },
   Linux: {
     name: "Linux",
     url: "https://www.linux.org/",
     icon: LogosLinuxTux
   },
-  // Design
-  Figma: {
-    name: "Figma",
-    url: "https://www.figma.com/",
-    icon: LogosFigma
+  MaterialUi: {
+    name: "Material UI",
+    url: "https://material-ui.com/",
+    icon: LogosMaterialUi
+  },
+  MSW: {
+    name: "MSW",
+    url: "https://mswjs.io/",
+    icon: LogosMswIcon
+  },
+  Nestjs: {
+    name: "Nest.js",
+    url: "https://nestjs.com/",
+    icon: LogosNestjs
+  },
+  Nextjs: {
+    name: "Next.js",
+    url: "https://nextjs.org/",
+    icon: LogosNextjsIcon
+  },
+  Nginx: {
+    name: "Nginx",
+    url: "https://www.nginx.com/",
+    icon: LogosNginx
+  },
+  Nodejs: {
+    name: "Node.js",
+    url: "https://nodejs.org/",
+    icon: LogosNodejsIconAlt
+  },
+  OpenCV: {
+    name: "OpenCV",
+    url: "https://opencv.org/",
+    icon: LogosOpencv
+  },
+  OWASPZAP: {
+    name: "OWASP ZAP",
+    url: "https://www.zaproxy.org/",
+    icon: SimpleIconsZap
+  },
+  Postman: {
+    name: "Postman",
+    url: "https://www.postman.com/",
+    icon: LogosPostmanIcon
+  },
+  Prisma: {
+    name: "Prisma",
+    url: "https://www.prisma.io/",
+    icon: LogosPrisma
+  },
+  Python: {
+    name: "Python",
+    url: "https://www.python.org/",
+    icon: LogosPython
+  },
+  React: {
+    name: "React",
+    url: "https://reactjs.org/",
+    icon: LogosReact
+  },
+  ReactHookForm: {
+    name: "React Hook Form",
+    url: "https://react-hook-form.com/",
+    icon: SimpleIconsReacthookform
+  },
+  ReactRouter: {
+    name: "React Router",
+    url: "https://reactrouter.com/",
+    icon: LogosReactRouter
+  },
+  Redux: {
+    name: "Redux",
+    url: "https://redux.js.org/",
+    icon: LogosRedux
+  },
+  SASS: {
+    name: "SASS",
+    url: "https://sass-lang.com/",
+    icon: LogosSass
+  },
+  ShadcnUi: {
+    name: "Shadcn UI",
+    url: "https://ui.shadcn.com/",
+    icon: SimpleIconsShadcnui
+  },
+  SonarQube: {
+    name: "Sonar Qube",
+    url: "https://www.sonarqube.org/",
+    icon: LogosSonarqube
+  },
+  StyledComponents: {
+    name: "Styled Components",
+    url: "https://styled-components.com/",
+    icon: VscodeIconsFileTypeStyled
+  },
+  Supabase: {
+    name: "Supabase",
+    url: "https://supabase.io/",
+    icon: LogosSupabaseIcon
+  },
+  TailwindCSS: {
+    name: "Tailwind CSS",
+    url: "https://tailwindcss.com/",
+    icon: LogosTailwindcssIcon
+  },
+  TypeScript: {
+    name: "Type Script",
+    url: "https://www.typescriptlang.org/",
+    icon: LogosTypescriptIcon
+  },
+  Vercel: {
+    name: "Vercel",
+    url: "https://vercel.com/",
+    icon: LogosVercelIcon
+  },
+  Vitest: {
+    name: "Vitest",
+    url: "https://vitest.dev/",
+    icon: LogosVitest
+  },
+  Vite: {
+    name: "Vite",
+    url: "https://vitejs.dev/",
+    icon: LogosVitejs
+  },
+  Windows: {
+    name: "Windows",
+    url: "https://www.microsoft.com/en-us/windows",
+    icon: LogosMicrosoftWindowsIcon
+  },
+  Zustand: {
+    name: "Zustand",
+    url: "https://zustand.surge.sh/",
+    icon: LogosZustand
   }
 }
 
@@ -364,10 +342,12 @@ export const technologiesOrder = [
   "Python",
   "Cplusplus",
   // Frameworks
+  "Nodejs",
   "React",
   "Angular",
   "Nextjs",
-  "Nodejs",
+  "Express",
+  "Nestjs",
   // Frontend
   "HTML",
   "CSS",
@@ -395,8 +375,6 @@ export const technologiesOrder = [
   "MSW",
   "Postman",
   // Backend
-  "Express",
-  "Nestjs",
   "Firebase",
   "Supabase",
   "Auth0",
